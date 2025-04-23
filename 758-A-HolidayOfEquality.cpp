07551bea7179577856d069dde1840cbeee888186
TSE121 <74487245+TSE121@users.noreply.github.com>
2025-04-23 23:43:42 +0530

    new problems

---
+++ 758-A-HolidayOfEquality.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int a[1000000];
+    int max_income = 0;
+    for (int i = 0; i < 1000000; i++)
+    {
+        cout << "enter the income of the " << i + 1 << " th person";
+        cin >> a[i];
+    }
+
+    for (int j = 0; j < 1000000; j++)
+    {
+    }
+    return 0;
+}
