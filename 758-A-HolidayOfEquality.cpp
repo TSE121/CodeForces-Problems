91994d92d629a51d804cfce982862bb5d8d4eae2
TSE121 <74487245+TSE121@users.noreply.github.com>
2025-04-24 00:00:25 +0530

    final

--- 758-A-HolidayOfEquality.cpp
+++ 758-A-HolidayOfEquality.cpp
@@ -5,16 +5,33 @@ using namespace std;
 int main()
 {
     int n;
+    cin >> n;
+
     int a[1000000];
-    int max_income = 0;
-    for (int i = 0; i < 1000000; i++)
+    int max_income = 0, welfare_spent = 0;
+    for (int i = 0; i < n; i++)
     {
-        cout << "enter the income of the " << i + 1 << " th person";
         cin >> a[i];
     }
 
-    for (int j = 0; j < 1000000; j++)
+    for (int j = 0; j < n; j++)
     {
+        if (a[j] > max_income)
+        {
+            max_income = a[j];
+        }
     }
-    return 0;
+
+    // cout << max_income << endl;
+
+    for (int k = 0; k < n; k++)
+    {
+        if (a[k] != max_income)
+        {
+            welfare_spent += (max_income - a[k]);
+        }
+    }
+
+    cout << welfare_spent << endl;
+    // return 0;
 }
